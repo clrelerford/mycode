7f6f7ff5bf165b28ef64c05a782b57786dbd9168
clrelerford <clrelerford@gmail.com>
2023-01-05 20:41:19 +0000

    While Looping

--- miniproject.py
+++ miniproject.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python3
+
 #FOODS TO DETERMINE YOUR ZODIAC SIGN
 
-{“Question: What would you like to drink?”,
-“water”: “earth”,
-“lemonade”: “water”,
-“milk”: “air”,
-“soda”: “fire”
+drinkq = {
+        “Question: What would you like to drink?”,
+        “water”: “earth”,
+        “lemonade”: “water”,
+        “milk”: “air”,
+        “soda”: “fire”
 }
 
-{“Question: What would you like as an appetizer?”,
-“fries”: “air”,
-“chips and guacamole”: “earth”,
-“buffalo wings”: “fire”,
-“potato skins”: “water”,
+appq = {
+        “Question: What would you like as an appetizer?”,
+        “fries”: “air”,
+        “chips and guacamole”: “earth”,
+        “buffalo wings”: “fire”,
+        “potato skins”: “water”,
 }
 
-{“Question: What would you like for an entrée?”,
-“alfredo pasta”: “earth”,
-“mac n cheese”: “air”,
-“cheeseburger”: “fire”,
-“pizza”: “water”,
+entreeq = {
+        “Question: What would you like for an entrée?”,
+        “alfredo pasta”: “earth”,
+        “mac n cheese”: “air”,
+        “cheeseburger”: “fire”,
+        “pizza”: “water”,
 }
 
