48f6c9ebdfecf0e83d664a7bb48c26606e676372
clrelerford <clrelerford@gmail.com>
2023-01-05 17:54:00 +0000

    Questionaire miniproject

---
+++ miniproject.py
@@ -0,0 +1,23 @@
+#FOODS TO DETERMINE YOUR ZODIAC SIGN
+
+{“Question: What would you like to drink?”,
+“water”: “earth”,
+“lemonade”: “water”,
+“milk”: “air”,
+“soda”: “fire”
+}
+
+{“Question: What would you like as an appetizer?”,
+“fries”: “air”,
+“chips and guacamole”: “earth”,
+“buffalo wings”: “fire”,
+“potato skins”: “water”,
+}
+
+{“Question: What would you like for an entrée?”,
+“alfredo pasta”: “earth”,
+“mac n cheese”: “air”,
+“cheeseburger”: “fire”,
+“pizza”: “water”,
+}
+
