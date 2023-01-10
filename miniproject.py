31084ba0b7f5bc5e81c154245dcdb432f2cb324c
clrelerford <clrelerford@gmail.com>
2023-01-10 23:42:36 +0000

    final push

--- miniproject.py
+++ miniproject.py
@@ -59,35 +59,44 @@ question6_list = list(question6.keys())
 
 print("What Olympic Sport Is Best Fit For Your Body Type?")
 
-time.sleep(3)
+time.sleep(1)
 
 def main():
         basketball = 0
         boxing = 0
         cycling = 0 
+        diving = 0
+        gymnastics = 0
         high_jumping = 0
         hurdling = 0
+        marathon = 0
         rowing = 0
         running = 0
         shooting = 0
+        soccer = 0
+        sprinting = 0
         swimming = 0
         tennis = 0
         throwing = 0
         volleyball = 0
         water_polo = 0
+        weightlifting = 0
+        wrestling = 0
         
-        print("Question 1...")
-        time.sleep(3)
+        print("Get ready!")
+        time.sleep(1)
+        print("Set!")
+        time.sleep(1)
+        print("Go!")
+        time.sleep(1)
         print(question1["Question"])
-        time.sleep(3)
         print(question1_list[1])
-        time.sleep(2)
         print(question1_list[2])
-        time.sleep(2)
+        print(question1_list[3])
         
         while True:
                 try:
-                        q1answer = input("Please choose letter A, B or C \n")
+                        q1answer = input("\nPlease choose letter A, B or C \n")
                         q1answer = q1answer.upper()
                         if q1answer not in ["A", "B", "C"]:
                                 print("Please make a valid choice between A, B or C")
@@ -114,8 +123,8 @@ def main():
                                 weightlifting += 1
 
                         break
-                except:
-                        print("Try again!")
+                except Exception as x:
+                        print("Try again!", x)
 
 
 main()
